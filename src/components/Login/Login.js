f847f03b78f9b0b0c6b7ecda30038d0e92461888
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2021-06-03 17:47:55 +0300

    create login form, connect yarn

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,39 @@
 import React from 'react'
+import s from './Logim.module.css'
+import { reduxForm, Field} from "redux-form";
 
-let Login = (props) => {
-    return (
-        <h1>Login</h1>
+const Login = (props) => {
+
+    let onSubmit = (formData) => {
+        console.log(formData)
+    }
+
+    return (<div>
+            <h1 className={s.title}>Login</h1>
+            <LoginReduxForm onSubmit={onSubmit} />
+        </div>
     )
 }
+
+const LoginForm = (props) => {
+    return (
+        <form className={s.form} onSubmit={props.handleSubmit}>
+            <div className={s.itemForm}>
+                <Field component={'input'} name={"login"} className={s.textInput} placeholder={'Login'}/>
+            </div>
+            <div className={s.itemForm}>
+                <Field component={'input'} name={"password"} className={s.textInput} placeholder={'Password'}/>
+            </div>
+            <div className={s.itemForm}>
+                <Field component={'input'} name={"rememberMe"} className={s.checkbox} type={'checkbox'}/> <span>Remember me</span>
+            </div>
+            <div className={s.itemForm}>
+                <button>Login</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm ({form: 'login'})(LoginForm)
 
 export default Login
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -5,6 +5,7 @@ import friendsReducer from "./Friends-reducer";
 import usersReducer from "./Users-Reducer";
 import AuthReducer from "./Auth-reducer";
 import thunkMiddleWare from "redux-thunk"
+import {reducer as formReduser} from 'redux-form'
 
 let reducers = combineReducers({
     profile: profileReducer,
@@ -12,6 +13,7 @@ let reducers = combineReducers({
     friendsPage: friendsReducer,
     usersPage: usersReducer,
     auth: AuthReducer,
+    form: formReduser
 })
 
 let store = createStore(reducers, applyMiddleware(thunkMiddleWare))
