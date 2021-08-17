7f3dc3d64085ac915d2a385c587cd7b203a6ed56
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2021-08-17 15:01:01 +0300

    add icons for sidebar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,24 @@
 import React from 'react';
 import s from './Navbar.module.css'
 import {NavLink} from "react-router-dom";
+import menu from '../../defaultData/Icon/menu-icon.png'
+import friends from '../../defaultData/Icon/FriendsIcon.png'
+import message from '../../defaultData/Icon/MessagesIcon.png'
+import profile from '../../defaultData/Icon/ProfileIcon.png'
+import users from '../../defaultData/Icon/usersIcons.png'
 
 const Navbar = () => {
     return (
         <div >
+            <div>
+                <img className={s.mainNavIcon} src={menu} alt=""/>
+                <span className={s.menuButton}>Menu</span>
+            </div>
             <ul>
-                <li className={s.LiItem}><NavLink activeClassName={s.active} className={s.item} to="/profile">Profile</NavLink> </li>
-                <li className={s.LiItem}><NavLink activeClassName={s.active} className={s.item} to="/messages">Messages</NavLink>  </li>
-                <li className={s.LiItem}><NavLink activeClassName={s.active} className={s.item} to="/users">Users</NavLink></li>
-                <li className={s.LiItem}><NavLink activeClassName={s.active} className={s.item} to="/friends">Friends</NavLink></li>
+                <li className={s.LiItem}><img className={s.navIcon} src={profile} alt=""/><NavLink activeClassName={s.active} className={s.item} to="/profile">Profile</NavLink> </li>
+                <li className={s.LiItem}><img className={s.navIcon} src={message} alt=""/><NavLink activeClassName={s.active} className={s.item} to="/messages">Messages</NavLink>  </li>
+                <li className={s.LiItem}><img className={s.navIcon} src={users} alt=""/><NavLink activeClassName={s.active} className={s.item} to="/users">Users</NavLink></li>
+                <li className={s.LiItem}><img className={s.navIcon} src={friends} alt=""/><NavLink activeClassName={s.active} className={s.item} to="/friends">Friends</NavLink></li>
             </ul>
         </div>
     );
