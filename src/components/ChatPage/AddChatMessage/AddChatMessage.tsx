dc57ccfa502cac08974bb36666e0a241d8163a02
SmorzhaniukCinis <smorzhaniuk.cinis@gmail.com>
2021-12-13 17:54:15 +0100

    continue fix bag))

--- src/components/ChatPage/AddChatMessage/AddChatMessage.tsx
+++ src/components/ChatPage/AddChatMessage/AddChatMessage.tsx
@@ -9,13 +9,13 @@ export const AddChatMessage = () => {
     const {register, handleSubmit, reset} = useForm();
     const onSubmit = (data: {ChatMessage:string}) => {
         socket.send(data.ChatMessage)
-        reset({ ChatMessage: '' });
+        reset();
     }
 
     return (
         <div className={s.container}>
             <form onSubmit={handleSubmit(onSubmit)}>
-                <TextField autoComplete={'off'} {...register("ChatMessage")} className={s.messageField} id="standard-basic"
+                <TextField   autoComplete={'off'} {...register("ChatMessage",{ required: true })} className={s.messageField} id="standard-basic"
                             label="Add your message" variant="standard"/>
                 <Button type="submit" className={s.sendButton} variant="contained"
                         endIcon={<img src={sendIcon} alt=""/>}>
